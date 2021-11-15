d72b406958aca94a40189b7d6ad3c8271219253b
docilefiasco <epshita.hs@gmail.com>
2021-11-15 14:54:06 +0530

    express generator

--- node-express/index.js
+++ node-express/index.js
@@ -5,6 +5,9 @@ const path = require('path')
 
 
 
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promotionRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -14,52 +17,11 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json()) ;
 //body parser deprecatrd
 
+app.use('/dishes',dishRouter); //mounting
+app.use('/leaders',leaderRouter);
+app.use('/promotions',promoRouter);
 
-app.all('/dishes',(req,res,next)=>
-{
-res.statusCode = 200;
- res.setHeader('Content-Type', 'text/plain');
- next();
- //will look for additional specifications that will match the endpoint dishes
- //carry on the modifications
-});
 
-app.get('/dishes',(req,res,next)=>{
-res.end('will send all dishes to you!');
-});
-
-app.post('/dishes',(req,res,next)=>{
-res.end('willl add the dish' + req.body.name + 'with details'+ req.body.description);
-});
-
-app.put('/dishes',(req,res,next)=>{
-  res.statusCode = 403;
-res.end('put not supported on dishes');
-});
-
-app.delete('/dishes',(req,res,next)=>{
-  
-res.end('deleting!');
-});
-
-app.get('/dishes/:dishId',(req,res,next)=>{
-res.end('will send details of dish:' + req.params.dishId+ 'to you!');
-});
-
-app.post('/dishes/:dishId',(req,res,next)=>{
-   res.statusCode = 403;
-res.end('not supported on dishes/'+ req.params.dishId);
-});
-
-app.put('/dishes/:dishId',(req,res,next)=>{
- 
-res.end('will update dish'+ req.params.dishId);
-});
-
-app.delete('/dishes/:dishId',(req,res,next)=>{
-  
-res.end('deleting!');
-});
 
 app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
