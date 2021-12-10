eb4ecf128b8e3061cec6e9d36a0321c07885f50a
docilefiasco <epshita.hs@gmail.com>
2021-12-10 09:59:15 +0530

    oauth

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,30 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var authenticate = require('./authenticate');
+var config = require('./config');
+const url = config.mongoUrl;
 
-function auth (req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-  }
-
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-      next(); // authorized
-  } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      next(err);
-  }
-}
 
 
 
@@ -38,18 +20,34 @@ const promoRouter = require('./routes/promotionRouter');
 var app = express();
 
 // view engine setup
-app.use(auth);
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser('12345-67890-09876-54321'));
+
+
+
+app.use(passport.initialize());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promoRouter);
@@ -57,25 +55,24 @@ app.use('/promotions', promoRouter);
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+
 const connect = mongoose.connect(url);
 
-connect.then((db)=>
-{
-console.log('connected correctly'),
-(err)=>{
-  console.log(err)
-}
+connect.then((db) => {
+  console.log('connected correctly'),
+    (err) => {
+      console.log(err)
+    }
 });
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
